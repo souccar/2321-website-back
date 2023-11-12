682badde0e23927fad0a124320e25a64e6e43dd7
Mohamad Tonbary <ttmohamad299@gmail.com>
2023-11-12 14:50:29 +0200

    Authorization

--- app/Domain/Authentication/Http/Controllers/AuthenticationController.php
+++ app/Domain/Authentication/Http/Controllers/AuthenticationController.php
@@ -5,38 +5,69 @@ namespace App\Domain\Authentication\Http\Controllers;
 use App\Helpers\AhcResponse;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthenticationController extends Controller
 {
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required',
-            'password' => 'required'
+        // $credentials = request(['email', 'password']);
+        $credentials = $request->only(['email', 'password']);
+
+        $validator = Validator::make($credentials, [
+            'email' => 'required|email',
+            'password' => 'required|min:8'
         ]);
-        $credentials = request(['email', 'password']);
+
+        if ($validator->fails()) {
+            return AhcResponse::sendResponse([], false, $validator->errors());
+        }
 
         if (!Auth::attempt($credentials)) {
-            return AhcResponse::sendResponse([], false, ['message' => 'Login Failed!']);
+            return AhcResponse::sendResponse([], false, ['user name or password is incorrect']);
         }
         $user = Auth::user();
-        // $user = $request->user();
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->plainTextToken;
+
+        $token = $user->createToken(
+            $user->name . '_' . Carbon::now(),
+            ['*'],
+            Carbon::now()->addDays(1)
+        )->plainTextToken;
 
         return AhcResponse::sendResponse([
             'user' => Auth::user(),
             'access_token' => $token,
-            'token_type' => 'Bearer'
         ]);
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
-        Auth::logout();
-        return AhcResponse::
-        sendResponse(['message' => 'Logged Out Successfully!'], true, []);
+        $token = \Laravel\Sanctum\PersonalAccessToken::findToken($request->header('Authorization'));
+        if ($token != null) {
+            $isDeleted = $token->delete();
+            if ($isDeleted) {
+                return AhcResponse::
+                    sendResponse(['Logged Out Successfully!'], true, []);
+            } else {
+                return AhcResponse::sendResponse([], false, ["Error"]);
+            }
+        } else {
+            return AhcResponse::sendResponse([], false, ["token not found"]);
+        }
+    }
+
+    public function getUserByToken(Request $request)
+    {
+        $token = \Laravel\Sanctum\PersonalAccessToken::findToken($request->header('Authorization'));
+        if ($token != null) {
+            if ($token->expires_at < Carbon::now()) {
+                return AhcResponse::sendResponse([], false, ['token has expired']);
+            }
+            return $token->tokenable;
+        }
+        return AhcResponse::sendResponse([], false, ['token not found']);
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -133,6 +133,111 @@ Route::delete('/deleteUser/{id}',[UserController::class,'destroy']);
 
 
 //Authentication
-Route::post('login',[AuthenticationController::class,'login']);
+Route::post('login',[AuthenticationController::class,'login'])->name('login');
 Route::post('logout',[AuthenticationController::class,'logout']);
+Route::get('getUser',[AuthenticationController::class,'getUserByToken']);
 
+
+// Route::group(['middleware' => 'authorize'], function () {
+
+//     //Authentication
+//     Route::post('logout', [AuthenticationController::class, 'logout']);
+//     Route::get('getUser', [AuthenticationController::class, 'getUserByToken']);
+
+//     //Customers
+//     Route::get('/customers', [CustomerController::class, 'index']);
+//     Route::post('/customers', [CustomerController::class, 'store']);
+//     Route::put('/customers/{id}', [CustomerController::class, 'edit']);
+//     Route::delete('/customers/{id}', [CustomerController::class, 'delete']);
+
+//     //News
+//     Route::get('/news', [NewsController::class, 'getAll']);
+//     Route::get('/news/{id}', [NewsController::class, 'getById']);
+//     Route::get('/getOnlyForHome', [NewsController::class, 'getOnlyForHome']);
+//     Route::get('/lastNews', [NewsController::class, 'getLastEightNews']);
+//     Route::post('/news', [NewsController::class, 'store']);
+//     Route::post('/news/{id}', [NewsController::class, 'edit']);
+//     Route::delete('/news/{id}', [NewsController::class, 'destroy']);
+
+//     //Categories
+//     Route::get('/categories', [CategoryController::class, 'getAll']);
+//     Route::get('/categories/{id}', [CategoryController::class, 'getById']);
+//     Route::post('/categories', [CategoryController::class, 'store']);
+//     Route::post('/categories/{id}', [CategoryController::class, 'edit']);
+//     Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+//     //Brands
+//     Route::get('/brands', [BrandController::class, 'getAll']);
+//     Route::get('/brands/{id}', [BrandController::class, 'getById']);
+//     Route::post('/brands', [BrandController::class, 'store']);
+//     Route::post('/brands/{id}', [BrandController::class, 'edit']);
+//     Route::delete('/brands/{id}', [BrandController::class, 'destroy']);
+
+//     //SkinTypes
+//     Route::get('/skinTypes', [SkinTypeController::class, 'getAll']);
+//     Route::get('/skinTypes/{id}', [SkinTypeController::class, 'getById']);
+//     Route::post('/skinTypes', [SkinTypeController::class, 'store']);
+//     Route::post('/skinTypes/{id}', [SkinTypeController::class, 'edit']);
+//     Route::delete('/skinTypes/{id}', [SkinTypeController::class, 'destroy']);
+
+//     //Products
+//     Route::get('/products', [ProductController::class, 'getAll']);
+//     Route::get('/products/{id}', [ProductController::class, 'getById']);
+//     Route::get('/productsByCategoryId', [ProductController::class, 'getByCategoryId']);
+//     Route::get('/productsByBrandId', [ProductController::class, 'getByBrandId']);
+//     Route::get('/productsBySkinTypeId', [ProductController::class, 'getBySkinTypeId']);
+//     Route::post('/products', [ProductController::class, 'store']);
+//     Route::post('/products/{id}', [ProductController::class, 'edit']);
+//     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+//     Route::post('/uploadProductImages', [ProductController::class, 'uploadProductImages']);
+//     Route::delete('/removeProductImages/{id}', [ProductImageController::class, 'destroy']);
+
+//     //Pages
+//     Route::post('/pages', [PageController::class, 'store']);
+//     Route::post('/pages/{id}', [PageController::class, 'edit']);
+//     Route::delete('/pages/{id}', [PageController::class, 'destroy']);
+//     Route::post('/uploadPageImage', [PageController::class, 'uploadPageImage']);
+//     Route::get('/getPagesForDrobdown', [PageController::class, 'getForDrobdown']);
+//     Route::get('/getAllPages', [PageController::class, 'getAll']);
+//     Route::get('/getPageBySlug/{slug}', [PageController::class, 'getPageBySlug']);
+//     Route::get('/getForEdit/{id}', [PageController::class, 'getForEdit']);
+//     Route::get('/getAssociatedTemplates/{pageId}', [PageController::class, 'getAssociatedTemplates']);
+
+//     //Templates
+//     Route::post('/templates', [TemplateController::class, 'store']);
+//     Route::post('/templates/{id}', [TemplateController::class, 'edit']);
+//     Route::delete('/templates/{id}', [TemplateController::class, 'destroy']);
+//     Route::post('/uploadTemplateImage', [TemplateController::class, 'uploadTemplateImage']);
+//     Route::get('/getTemplatesForDrobdown', [TemplateController::class, 'getForDrobdown']);
+//     Route::get('/getAllTemplates', [TemplateController::class, 'getAll']);
+//     Route::get('/getAllForPage', [TemplateController::class, 'getAllForPage']);
+//     Route::get('/getWithChildren', [TemplateController::class, 'getWithChildren']);
+
+//     //PageTamplates
+//     Route::post('/pageTemplates', [PageTemplateController::class, 'store']);
+//     Route::post('/pageTemplates/{id}', [PageTemplateController::class, 'edit']);
+//     Route::delete('/pageTemplates/{id}', [PageTemplateController::class, 'destroy']);
+
+
+//     //ContactUs
+//     Route::get('/allContacts', [ContactUsController::class, 'getAll']);
+//     Route::get('/contacts/{id}', [ContactUsController::class, 'getById']);
+//     Route::post('/contactUs', [ContactUsController::class, 'store']);
+//     Route::delete('/contacts/{id}', [ContactUsController::class, 'destroy']);
+
+
+//     //Slider
+//     Route::post('/Sliders', [SliderController::class, 'store']);
+//     Route::post('/Sliders/{id}', [SliderController::class, 'edit']);
+//     Route::delete('/Sliders/{id}', [SliderController::class, 'destroy']);
+//     Route::post('/uploadSliderImage', [SliderController::class, 'uploadSliderImage']);
+//     Route::get('/getAllSliders', [SliderController::class, 'getAll']);
+
+
+//     //User
+//     Route::get('users/{count}', [UserController::class, 'GetAll']);
+//     Route::get('user/{id}', [UserController::class, 'getById']);
+//     Route::post('/register', [UserController::class, 'register']);
+//     Route::put('/editUser/{id}', [UserController::class, 'edit']);
+//     Route::delete('/deleteUser/{id}', [UserController::class, 'destroy']);
+// });
